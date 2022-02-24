3c8f2b749bcf645b6063dc9c837b829509e098c9
toMilanesi <tomi.milanesi@gmail.com>
2022-02-24 19:11:17 +1100

    modified script and css

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const pixContainer = document.getElementById("pix-container");
-
+const diezButton = document.getElementById("diez");
+const treintaButton = document.getElementById("treinta");
+const sesentaButton = document.getElementById("sesenta");
 const fragment = document.createDocumentFragment();
 
-//Crea el grid
-function createPix (cantidad){
+function crearPix(cantidad, pixclass){
+    let clasePix = pixclass;
     for(let i = 0; i < cantidad; i++){
-        const pix = document.createElement("div");
-        pix.classList.add("pixel");
-        fragment.appendChild(pix);
+        const div = document.createElement("div");
+        div.className(`${clasePix}`);
+        fragment.appendChild(div);
     }
     pixContainer.appendChild(fragment);
 }
-// cambia el bg de la class
-pixContainer.addEventListener("mouseover", e =>{
-    if(e.target.className == "pixel"){
-        e.target.classList.remove("pixel");
-        e.target.classList.add("pixelOn");
-    }
-})
 
-createPix(30);
+diezButton.addEventListener("click", () =>{
+    pixContainer.className("diezPixContainer");
+    crearPix(256, "treintaPix")
+
+})
