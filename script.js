75498f1efec7b1011cdeda696d42a3cad61dc375
toMilanesi <tomi.milanesi@gmail.com>
2022-03-01 16:05:08 +1100

    fixed script, can draw in 16, 32, 64

--- script.js
+++ script.js
@@ -4,18 +4,57 @@ const treintaButton = document.getElementById("treinta");
 const sesentaButton = document.getElementById("sesenta");
 const fragment = document.createDocumentFragment();
 
-function crearPix(cantidad, pixclass){
-    let clasePix = pixclass;
-    for(let i = 0; i < cantidad; i++){
+function pixOn(){
+    pixContainer.addEventListener("mouseover", function(e){
+        if(e.target.className === "diezPix"){
+            e.target.className = "diezPixOn";
+        }
+        else if(e.target.className === "treintaPix"){
+            e.target.className = "treintaPixOn";
+        }
+        else if(e.target.className === "sesentaPix"){
+            e.target.className = "sesentaPixOn";
+        }
+        
+    })
+    
+}
+
+diezButton.addEventListener("click", function(e) {
+    pixContainer.innerHTML = "";
+    pixContainer.className = "diezPixContainer";
+
+    for(let i = 0; i < 256; i++){
         const div = document.createElement("div");
-        div.className(`${clasePix}`);
+        div.className = "diezPix";
+        fragment.appendChild(div);
+    }
+    pixContainer.appendChild(fragment)
+
+    pixOn();
+
+})
+
+treintaButton.addEventListener("click", e =>{
+    pixContainer.innerHTML = "";
+    pixContainer.className = "treintaPixContainer";
+
+    for(let i = 0; i < 1024; i++){
+        let div = document.createElement("div");
+        div.className = "treintaPix";
         fragment.appendChild(div);
     }
     pixContainer.appendChild(fragment);
-}
+})
 
-diezButton.addEventListener("click", () =>{
-    pixContainer.className("diezPixContainer");
-    crearPix(256, "treintaPix")
+sesentaButton.addEventListener("click", e =>{
+    pixContainer.innerHTML = "";
+    pixContainer.className = "sesentaPixContainer";
 
+    for(let i = 0; i < 4096; i++){
+        let div = document.createElement("div");
+        div.className = "sesentaPix";
+        fragment.appendChild(div);
+    }
+    pixContainer.appendChild(fragment);
 })
